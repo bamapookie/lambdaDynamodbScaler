f1924c5db3baaccf4fd9a15119e4ce8076bf4988
shawn.kovalchick <shawn.kovalchick@mobileposse.com>
2017-06-15 14:54:31 -0400

    Fix formatting and merge changes

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # lambdaDynamodbScaler
-An AWS Lambda function to scale DynamoDB tables on an hourly schedule
+An AWS Lambda function to scale DynamoDB tables and global secondary indexes on an hourly schedule
 
-
-More info can be found here : https://medium.com/@quodlibet_be/using-aws-lambda-scheduled-tasks-to-scale-dynamodb-c65a336aca4f
+More info can be found
+[here](https://medium.com/@quodlibet_be/using-aws-lambda-scheduled-tasks-to-scale-dynamodb-c65a336aca4f).
 
 
 
--- src/main/java/be/quodlibet/lambdadynamodbscaler/Scaler.java
+++ src/main/java/be/quodlibet/lambdadynamodbscaler/Scaler.java
@@ -26,13 +26,17 @@ import java.util.Properties;
 /**
  * @author Dries Horions <dries@quodlibet.be>
  */
-public class Scaler {
-    //    private static final String access_key_id = "ACCESSKEY";
-    //    private static final String secret_access_key = "SECRET";
-    private static final String configBucketName = Optional.ofNullable(System.getenv("BUCKETNAME")).orElse("default-bucket-name-replace");
+public class Scaler
+{
+    private static final String configBucketName = Optional
+           .ofNullable(System.getenv("BUCKETNAME"))
+           .orElse("default-bucket-name-replace");
     private static final String configKey = "scaler.properties";
-    private static final Regions region = Regions
-            .fromName(Optional.ofNullable(System.getenv("AWS_DEFAULT_REGION")).orElse("us-east-1"));
+    private static final Regions region = Regions.fromName(
+           Optional
+                  .ofNullable(System.getenv("AWS_DEFAULT_REGION"))
+                  .orElse("us-east-1")
+    );
 
     private Properties ScalingProperties;
     private AmazonS3 s3Client;
@@ -40,8 +44,10 @@ public class Scaler {
     private DynamoDB dynamoDB;
     private LambdaLogger log;
 
-    public Response scale(Object input, Context context) {
-        if (context != null) {
+    public Response scale(Object input, Context context)
+    {
+        if (context != null)
+        {
             log = context.getLogger();
         }
         setup();
@@ -51,15 +57,17 @@ public class Scaler {
         String message = "Scaling for hour : " + hour + "\n";
         log(message);
         //Get the table names
-        if (ScalingProperties.containsKey("tablenames")) {
+        if (ScalingProperties.containsKey("tablenames"))
+        {
             String value = (String) ScalingProperties.get("tablenames");
             String[] tableNames = value.split(",");
-            for (String tableName : tableNames) {
+            for (String tableName : tableNames)
+            {
                 //Check if there is a change requested for this hour
                 String readProp = hour + "." + tableName + ".read";
                 String writeProp = hour + "." + tableName + ".write";
                 if (ScalingProperties.containsKey(readProp)
-                    && ScalingProperties.containsKey(writeProp))
+                       && ScalingProperties.containsKey(writeProp))
                 {
                     String readCapacity = ScalingProperties.getProperty(readProp);
                     String writeCapacity = ScalingProperties.getProperty(writeProp);
@@ -92,8 +100,7 @@ public class Scaler {
                     //Check if there is a change requested for this hour
                     String readProp = hour + "." + indexName + ".read";
                     String writeProp = hour + "." + indexName + ".write";
-                    if (ScalingProperties.containsKey(readProp)
-                        && ScalingProperties.containsKey(writeProp))
+                    if (ScalingProperties.containsKey(readProp) && ScalingProperties.containsKey(writeProp))
                     {
                         String readCapacity = ScalingProperties.getProperty(readProp);
                         String writeCapacity = ScalingProperties.getProperty(writeProp);
@@ -126,36 +133,35 @@ public class Scaler {
      *
      * @param message
      */
-    private void log(String message) {
-        if (log != null) {
+    private void log(String message)
+    {
+        if (log != null)
+        {
             log.log(message);
-        } else {
+        }
+        else
+        {
             System.out.println(message);
         }
     }
 
-    private String scaleTable(String tableName, Long readCapacity, Long writeCapacity) {
+    private String scaleTable(String tableName, Long readCapacity, Long writeCapacity)
+    {
         Table table = dynamoDB.getTable(tableName);
         ProvisionedThroughput tp = new ProvisionedThroughput();
         tp.setReadCapacityUnits(readCapacity);
         tp.setWriteCapacityUnits(writeCapacity);
         TableDescription d = table.describe();
-        if (!Objects.equals(d.getProvisionedThroughput().getReadCapacityUnits(), readCapacity) || !Objects
-                .equals(d.getProvisionedThroughput().getWriteCapacityUnits(), writeCapacity)) {
+        if (!Objects.equals(d.getProvisionedThroughput().getReadCapacityUnits(), readCapacity)
+               || !Objects.equals(d.getProvisionedThroughput().getWriteCapacityUnits(), writeCapacity))
+        {
             d = table.updateTable(tp);
-            return tableName
-                   + "\nRequested read/write : "
-                   + readCapacity
-                   + "/"
-                   + writeCapacity
-                   + "\nCurrent read/write :"
-                   + d.getProvisionedThroughput().getReadCapacityUnits()
-                   + "/"
-                   + d.getProvisionedThroughput().getWriteCapacityUnits()
-                   + "\nStatus : "
-                   + d.getTableStatus()
-                   + "\n";
-        } else {
+            return tableName + "\nRequested read/write : " + readCapacity + "/" + writeCapacity
+                   + "\nCurrent read/write :" + d.getProvisionedThroughput().getReadCapacityUnits() + "/"
+                   + d.getProvisionedThroughput().getWriteCapacityUnits() + "\nStatus : " + d.getTableStatus() + "\n";
+        }
+        else
+        {
             return tableName + "\n Requested throughput equals current throughput\n";
         }
     }
@@ -167,14 +173,18 @@ public class Scaler {
         tp.setReadCapacityUnits(readCapacity);
         tp.setWriteCapacityUnits(writeCapacity);
         TableDescription d = table.describe();
-        for (GlobalSecondaryIndexDescription indexDescription : d.getGlobalSecondaryIndexes()) {
-            if (Objects.equals(indexDescription.getIndexName(), indexName)) {
+        for (GlobalSecondaryIndexDescription indexDescription : d.getGlobalSecondaryIndexes())
+        {
+            if (Objects.equals(indexDescription.getIndexName(), indexName))
+            {
                 if (!Objects.equals(indexDescription.getProvisionedThroughput().getReadCapacityUnits(), readCapacity)
-                        || !Objects.equals(indexDescription.getProvisionedThroughput().getWriteCapacityUnits(), writeCapacity)) {
+                       || !Objects.equals(indexDescription.getProvisionedThroughput().getWriteCapacityUnits(), writeCapacity))
+                {
                     d = table.getIndex(indexName).updateGSI(tp);
                     return tableName + ":" + indexName + "\nRequested read/write : " + readCapacity + "/" + writeCapacity
-                            + "\nCurrent read/write :" + d.getProvisionedThroughput().getReadCapacityUnits() + "/" + d.getProvisionedThroughput().getWriteCapacityUnits()
-                            + "\nStatus : " + d.getTableStatus() + "\n";
+                           + "\nCurrent read/write :" + d.getProvisionedThroughput().getReadCapacityUnits() + "/"
+                           + d.getProvisionedThroughput().getWriteCapacityUnits() + "\nStatus : " + d.getTableStatus()
+                           + "\n";
                 }
                 else
                 {
@@ -187,29 +197,30 @@ public class Scaler {
 
     private void setup()
     {
-        //Setup credentials
-        if (awsCreds == null)
-        {
-            awsCreds = new BasicAWSCredentials(access_key_id, secret_access_key);
-        }
         //Setup S3 client
-        if (s3Client == null) {
+        if (s3Client == null)
+        {
             s3Client = new AmazonS3Client();
         }
         //Setup DynamoDB client
-        if (clnt == null) {
+        if (clnt == null)
+        {
             clnt = new AmazonDynamoDBClient(new DefaultAWSCredentialsProviderChain());
             dynamoDB = new DynamoDB(clnt);
             clnt.setRegion(Region.getRegion(region));
         }
         //Load properties from S3
-        if (ScalingProperties == null) {
-            try {
+        if (ScalingProperties == null)
+        {
+            try
+            {
                 ScalingProperties = new Properties();
                 S3Object object = s3Client.getObject(new GetObjectRequest(configBucketName, configKey));
                 S3ObjectInputStream stream = object.getObjectContent();
                 ScalingProperties.load(stream);
-            } catch (IOException ex) {
+            }
+            catch (IOException ex)
+            {
                 log("Failed to read config file : " + configBucketName + "/" + configKey + "(" + ex.getMessage() + ")");
             }
         }
